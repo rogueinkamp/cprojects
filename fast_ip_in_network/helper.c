0e66d7ae3256e66afebd7805f4113f2b8f46de42
evan.roggenkamp <evan.roggenkamp@arvig.com>
2022-07-08 11:49:54 -0500

    Working on building a module I can call from python

--- fast_ip_in_network/helper.c
+++ fast_ip_in_network/helper.c
@@ -1,4 +1,5 @@
- #include <stdio.h>
+ #include <stdint.h>
+#include <stdio.h>
 #include <string.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
@@ -6,17 +7,24 @@
 
 #include <stdbool.h>
 
+struct NetworkAndMaskBits {
+    char network_address[INET_ADDRSTRLEN];
+    int mask_bits;
+};
 
-bool ip_in_network(const char* addr, const char* net) {
+
+unsigned ip_in_network(const char* addr, const char* net) {
 	struct in_addr ip_addr;
 	int ret = inet_aton(addr, &ip_addr);
 	if (!ret)
+        printf("ret -> %i\n", ret);
 		return false;
 	char network[32];
 	strncpy(network, net, strlen(net));
 
 	char* slash = strstr(network, "/");
 	if (!slash)
+        printf("no slash\n");
 		return false;
 	int mask_len = atoi(slash + 1);
 
@@ -24,41 +32,72 @@ bool ip_in_network(const char* addr, const char* net) {
 	struct in_addr net_addr;
 	int ret2= inet_aton(network, &net_addr);
 	if (!ret2)
+        printf("ret -> %i\n", ret);
 		return false;
 
 	unsigned ip_bits = ip_addr.s_addr;
 	unsigned net_bits = net_addr.s_addr;
 	unsigned netmask = net_bits & ((1 << mask_len) - 1);
-	return (ip_bits & netmask) == net_bits;
+	unsigned res = (ip_bits & netmask) == net_bits;
+    printf("RES -> %i", res);
+    return res;
  }
 
 
-bool fast_ip_in_network(char ip[INET_ADDRSTRLEN], char ip_cidr[INET_ADDRSTRLEN], uint32_t prefix, char* network_address, uint32_t mask_bits) {
+struct NetworkAndMaskBits get_network_addr_and_mask_bits(const char *interface_ip_address_string) {
+    // do stuff
+    char ip_address_cidr_string[INET_ADDRSTRLEN];
+    char *prefix;
+    uint32_t prefix_int;
+    uint32_t converted_ip_int;
+    uint32_t network_bits;
+    uint32_t shifted;
+    struct in_addr ip_addr;
+    struct NetworkAndMaskBits r;
+	char ipNet[INET_ADDRSTRLEN];
 	uint32_t ip_int;
 	uint32_t netb;
-	uint32_t MASK_TOTAL_BITS = 32;
-	inet_pton(AF_INET, ip, &ip_int);
+	uint32_t mask_bits = 32;
+    strcpy(ip_address_cidr_string, interface_ip_address_string);
+    strtok_r(ip_address_cidr_string, "/", &prefix);
+    sscanf(prefix, "%d", &prefix_int);
+	inet_pton(AF_INET, ip_address_cidr_string, &ip_int);
 	ip_int = ntohl(ip_int);
+	/* printBits(ip_int); */
 	unsigned long mask = (
-		0xFFFFFFFF << (MASK_TOTAL_BITS - mask_bits)) & 0xFFFFFFFF;
+		0xFFFFFFFF << (mask_bits - prefix_int)) & 0xFFFFFFFF;
+	/* printBits(mask); */
 	netb = ip_int & mask;
 	netb = ntohl(netb);
+	/* printBits(netb); */
 	inet_ntop(
 		AF_INET,
 		&netb,
-		ip_cidr,
+		ipNet,
 		INET_ADDRSTRLEN
 	);
-	printf(
-		"network for %s and mask %d: %s",
-		ip,
-		prefix,
-		ip_cidr
-	);
-    return ip_in_network(ip_cidr, prefix);
+    printf(
+        "ORINGINAL: %s HOST_PORTION: '%s'  MASK_BITS: '%d' NETWORK_BITS: %d SHIFTED: %d, NETWORK_ADDRESS: %s\n",
+        interface_ip_address_string,
+        ip_address_cidr_string,
+        prefix_int,
+        network_bits,
+        shifted,
+        ipNet
+    );
+    r.mask_bits = mask_bits;
+    strcpy(r.network_address, ipNet);
+    return r;
 }
 
 
 int main() {
-    return 0;
+    char test_ip_cidr[INET_ADDRSTRLEN] = "192.168.0.71/27";
+    char test_ip_network[INET_ADDRSTRLEN] = "192.168.0.64/27";
+    struct NetworkAndMaskBits net_addr_and_mask = get_network_addr_and_mask_bits(test_ip_cidr);
+    /* char mask_bits_char[100]; */
+    /* sprintf(mask_bits_char, "%d", net_addr_and_mask.mask_bits); */
+    unsigned _ip_in_network = ip_in_network(test_ip_cidr, net_addr_and_mask.network_address);
+    printf("IN NETWORK %i", _ip_in_network);
+    return 1;
 }
